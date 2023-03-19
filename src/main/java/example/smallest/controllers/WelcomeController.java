2883bfe86324a87345819fb9a8c3611c9770e8c9
sunny <sunny.seera@hdruk.ac.uk>
2023-03-19 12:30:39 +0000

    update README

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -16,6 +16,6 @@ public class WelcomeController {
 		
 		
 		
-		return "Hello World! From Sunny Seera"; //"application/json" mean this is a text not a redirect
+		return "Hello World! From Sunny Seera!!"; //"application/json" mean this is a text not a redirect
 	}
 }
